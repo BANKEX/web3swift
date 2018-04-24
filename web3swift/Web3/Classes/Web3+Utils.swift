d84283577b3e37f55f0865afef541ba59bd354ad
Kevin <kevin@KevindeMacBook-Pro.local>
2018-04-24 09:57:57 +0800

    Fix  signature with 2 hash

--- web3swift/Web3/Classes/Web3+Utils.swift
+++ web3swift/Web3/Classes/Web3+Utils.swift
@@ -180,6 +180,18 @@ extension Web3.Utils {
         return Web3.Utils.publicToAddress(publicKey)
     }
     
+    static public func personalECRecover2Hash(_ personalMessage: Data, signature: Data) -> EthereumAddress? {
+        if signature.count != 65 { return nil}
+        let rData = signature[0..<32].bytes
+        let sData = signature[32..<64].bytes
+        let vData = signature[64]
+        guard let signatureData = SECP256K1.marshalSignature(v: vData, r: rData, s: sData) else {return nil}
+        guard let hash = Web3.Utils.hashPersonalMessage(personalMessage) else {return nil}
+        let hash1 = hash.sha3(.keccak256)
+        guard let publicKey = SECP256K1.recoverPublicKey(hash: hash1, signature: signatureData) else {return nil}
+        return Web3.Utils.publicToAddress(publicKey)
+    }
+    
     static public func hashECRecover(hash: Data, signature: Data) -> EthereumAddress? {
         if signature.count != 65 { return nil}
         let rData = signature[0..<32].bytes
