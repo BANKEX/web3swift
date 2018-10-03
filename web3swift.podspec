6283602fbccef3f4e422352279a6f0dd65020b60
Dmitry <d@kozlov.ru>
2018-10-03 14:12:34 +0300

    Some cosmetics removed json file from tests #153 Updated swift version in podspec

--- web3swift.podspec
+++ web3swift.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
 s.name             = "web3swift"
-s.version          = "1.1.5"
+s.version          = "1.1.6"
 s.summary          = "Web3 implementation in vanilla Swift for iOS ans macOS"
 
 s.description      = <<-DESC
@@ -13,7 +13,7 @@ s.author           = { "Alex Vlasov" => "av@bankexfoundation.org" }
 s.source           = { :git => 'https://github.com/bankex/web3swift.git', :tag => s.version.to_s }
 s.social_media_url = 'https://twitter.com/shamatar'
 
-s.swift_version = '4.1'
+s.swift_version = '4.2'
 s.module_name = 'web3swift'
 s.ios.deployment_target = "9.0"
 s.osx.deployment_target = "10.11"
--- web3swift/Promises/Classes/Promise+Batching.swift
+++ web3swift/Promises/Classes/Promise+Batching.swift
@@ -34,8 +34,8 @@ public class JSONRPCrequestDispatcher {
         var pendingTrigger: Guarantee<Void>?
         var provider: Web3Provider
         var queue: DispatchQueue
-        var lockQueue : DispatchQueue
-        var triggered : Bool = false
+        var lockQueue: DispatchQueue
+        var triggered: Bool = false
         func add(_ request: JSONRPCrequest, maxWaitTime: TimeInterval) throws -> Promise<JSONRPCresponse> {
             if self.triggered {
                 throw Web3Error.nodeError("Batch is already in flight")
@@ -62,12 +62,10 @@ public class JSONRPCrequestDispatcher {
         
         func trigger() {
             self.lockQueue.async {
-                if self.triggered {
-                    return
-                }
+                guard !self.triggered else { return }
                 self.triggered = true
                 let requestsBatch = JSONRPCrequestBatch(requests: self.requests)
-                _ = self.provider.sendAsync(requestsBatch, queue: self.queue).done(on: self.queue){batch in
+                self.provider.sendAsync(requestsBatch, queue: self.queue).done(on: self.queue) { batch in
                     for response in batch.responses {
                         if self.promisesDict[UInt64(response.id)] == nil {
                             for k in self.promisesDict.keys {
@@ -80,7 +78,7 @@ public class JSONRPCrequestDispatcher {
                         let promise = self.promisesDict[UInt64(response.id)]!
                         promise.resolver.fulfill(response)
                     }
-                }.catch(on:self.queue) {err in
+                }.catch(on: self.queue) { err in
                     for k in self.promisesDict.keys {
                         self.promisesDict[k]?.resolver.reject(err)
                     }
@@ -125,9 +123,9 @@ public class JSONRPCrequestDispatcher {
                     do {
                         let batch = try self.getBatch()
                         let internalPromise = try batch.add(request, maxWaitTime: self.MAX_WAIT_TIME)
-                        internalPromise.done(on: self.queue) {resp in
+                        internalPromise.done(on: self.queue) { resp in
                             seal.fulfill(resp)
-                        }.catch(on: self.queue){err in
+                        }.catch(on: self.queue) { err in
                             seal.reject(err)
                         }
                     } catch {
