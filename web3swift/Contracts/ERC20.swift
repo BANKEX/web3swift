4c57bb53ad5ce2bef4f6797db8f414db7707b4d5
Dmitry <d@kozlov.ru>
2018-10-17 21:34:46 +0300

    Added ERC20 Contract

--- web3swift/Contracts/ERC20.swift
+++ web3swift/Contracts/ERC20.swift
@@ -10,16 +10,30 @@ import Foundation
 import BigInt
 import PromiseKit
 
-//class ERC20 {
-//    let address: EthereumAddress
-//    init(address: EthereumAddress) {
-//        self.address = address
-//    }
-//    var name: String {
-//        let function = UnsafeSolidityFunction("name()")
-//        let data = function.data()
-//        let transaction = EthereumTransaction(to: address, data: data, options: .default)
-//        let request = TransactionIntermediate(transaction: transaction, web3: .default, contract: <#T##ContractProtocol#>, method: "name", options: .default)
-//        
-//    }
-//}
+public class ERC20 {
+    public let address: EthereumAddress
+    public init(address: EthereumAddress) {
+        self.address = address
+    }
+    public func name() throws -> String {
+        return try address.call("name()").string()
+    }
+    public func totalSupply() throws -> BigUInt {
+        return try address.call("totalSupply()").uint256()
+    }
+    public func balance(of user: EthereumAddress) throws -> BigUInt {
+        return try address.call("balanceOf(address)",user).uint256()
+    }
+    public func allowance(from owner: EthereumAddress, to spender: EthereumTransaction) throws -> BigUInt {
+        return try address.call("allowance(address,address)",owner,spender).uint256()
+    }
+    public func transfer(to user: EthereumAddress, value: BigUInt) throws -> TransactionSendingResult {
+        return try address.send("transfer(address,uint256)",user,value)
+    }
+    public func approve(to user: EthereumAddress, value: BigUInt) throws -> TransactionSendingResult {
+        return try address.send("approve(address,uint256)",user,value)
+    }
+    public func transfer(from: EthereumAddress, to: EthereumAddress, value: BigUInt) throws -> TransactionSendingResult {
+        return try address.send("transferFrom(address,address,uint256)",from,to,value)
+    }
+}
