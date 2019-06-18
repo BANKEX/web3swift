d877431bb2a6b326a731a60399f51acd1d7c180e
fengjunping@126.com <fengjunping@126.com>
2019-06-18 12:19:49 +0800

    Change to ctxc

--- Sources/web3swift/Web3/Web3+Methods.swift
+++ Sources/web3swift/Web3/Web3+Methods.swift
@@ -21,44 +21,44 @@ public struct JsonRpcMethod: Encodable, Equatable {
         self.parameters = parameters
     }
 	
-	/// eth_gasPrice method
-    public static let gasPrice = JsonRpcMethod(api: "eth_gasPrice", parameters: 0)
-	/// eth_blockNumber method
-    public static let blockNumber = JsonRpcMethod(api: "eth_blockNumber", parameters: 0)
+	/// ctxc_gasPrice method
+    public static let gasPrice = JsonRpcMethod(api: "ctxc_gasPrice", parameters: 0)
+	/// ctxc_blockNumber method
+    public static let blockNumber = JsonRpcMethod(api: "ctxc_blockNumber", parameters: 0)
 	/// net_version method
     public static let getNetwork = JsonRpcMethod(api: "net_version", parameters: 0)
-	/// eth_sendRawTransaction method
-    public static let sendRawTransaction = JsonRpcMethod(api: "eth_sendRawTransaction", parameters: 1)
-	/// eth_sendTransaction method
-    public static let sendTransaction = JsonRpcMethod(api: "eth_sendTransaction", parameters: 1)
-	/// eth_estimateGas method
-    public static let estimateGas = JsonRpcMethod(api: "eth_estimateGas", parameters: 1)
-	/// eth_call method
-    public static let call = JsonRpcMethod(api: "eth_call", parameters: 2)
-	/// eth_getTransactionCount method
-    public static let getTransactionCount = JsonRpcMethod(api: "eth_getTransactionCount", parameters: 2)
-	/// eth_getBalance method
-    public static let getBalance = JsonRpcMethod(api: "eth_getBalance", parameters: 2)
-	/// eth_getCode method
-    public static let getCode = JsonRpcMethod(api: "eth_getCode", parameters: 2)
-	/// eth_getStorageAt method
-    public static let getStorageAt = JsonRpcMethod(api: "eth_getStorageAt", parameters: 2)
-	/// eth_getTransactionByHash method
-    public static let getTransactionByHash = JsonRpcMethod(api: "eth_getTransactionByHash", parameters: 1)
-	/// eth_getTransactionReceipt method
-    public static let getTransactionReceipt = JsonRpcMethod(api: "eth_getTransactionReceipt", parameters: 1)
-	/// eth_accounts method
-    public static let getAccounts = JsonRpcMethod(api: "eth_accounts", parameters: 0)
-	/// eth_getBlockByHash method
-    public static let getBlockByHash = JsonRpcMethod(api: "eth_getBlockByHash", parameters: 2)
-	/// eth_getBlockByNumber method
-    public static let getBlockByNumber = JsonRpcMethod(api: "eth_getBlockByNumber", parameters: 2)
-	/// eth_sign method
-    public static let personalSign = JsonRpcMethod(api: "eth_sign", parameters: 1)
+	/// ctxc_sendRawTransaction method
+    public static let sendRawTransaction = JsonRpcMethod(api: "ctxc_sendRawTransaction", parameters: 1)
+	/// ctxc_sendTransaction method
+    public static let sendTransaction = JsonRpcMethod(api: "ctxc_sendTransaction", parameters: 1)
+	/// ctxc_estimateGas method
+    public static let estimateGas = JsonRpcMethod(api: "ctxc_estimateGas", parameters: 1)
+	/// ctxc_call method
+    public static let call = JsonRpcMethod(api: "ctxc_call", parameters: 2)
+	/// ctxc_getTransactionCount method
+    public static let getTransactionCount = JsonRpcMethod(api: "ctxc_getTransactionCount", parameters: 2)
+	/// ctxc_getBalance method
+    public static let getBalance = JsonRpcMethod(api: "ctxc_getBalance", parameters: 2)
+	/// ctxc_getCode method
+    public static let getCode = JsonRpcMethod(api: "ctxc_getCode", parameters: 2)
+	/// ctxc_getStorageAt method
+    public static let getStorageAt = JsonRpcMethod(api: "ctxc_getStorageAt", parameters: 2)
+	/// ctxc_getTransactionByHash method
+    public static let getTransactionByHash = JsonRpcMethod(api: "ctxc_getTransactionByHash", parameters: 1)
+	/// ctxc_getTransactionReceipt method
+    public static let getTransactionReceipt = JsonRpcMethod(api: "ctxc_getTransactionReceipt", parameters: 1)
+	/// ctxc_accounts method
+    public static let getAccounts = JsonRpcMethod(api: "ctxc_accounts", parameters: 0)
+	/// ctxc_getBlockByHash method
+    public static let getBlockByHash = JsonRpcMethod(api: "ctxc_getBlockByHash", parameters: 2)
+	/// ctxc_getBlockByNumber method
+    public static let getBlockByNumber = JsonRpcMethod(api: "ctxc_getBlockByNumber", parameters: 2)
+	/// ctxc_sign method
+    public static let personalSign = JsonRpcMethod(api: "ctxc_sign", parameters: 1)
 	/// personal_unlockAccount method
     public static let unlockAccount = JsonRpcMethod(api: "personal_unlockAccount", parameters: 1)
-	/// eth_getLogs method
-    public static let getLogs = JsonRpcMethod(api: "eth_getLogs", parameters: 1)
+	/// ctxc_getLogs method
+    public static let getLogs = JsonRpcMethod(api: "ctxc_getLogs", parameters: 1)
 	/// txpool_status method
     public static let txPoolStatus = JsonRpcMethod(api: "txpool_status", parameters: 0)
 	/// txpool_inspect method
